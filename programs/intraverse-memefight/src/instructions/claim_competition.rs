cd1b9111db538ea05ba6ac75c600d019a28ad981
Giacomo Cavicchioli <cavicchioli.giacomo@gmail.com>
2024-12-02 17:19:17 +0100

    fix: fixed signer for claim

--- programs/intraverse-memefight/src/instructions/claim_competition.rs
+++ programs/intraverse-memefight/src/instructions/claim_competition.rs
@@ -3,13 +3,13 @@ use anchor_lang::prelude::*;
 
 #[derive(Accounts)]
 pub struct ClaimCompetitionContext<'info> {
-    #[account(mut, has_one = owner)]
+    #[account(mut)]
     pub competition: Account<'info, Competition>,
 
     /// * * * * * * * * * * * *
 
     #[account(mut)]
-    pub owner: Signer<'info>,
+    pub signer: Signer<'info>,
 }
 
 pub fn handler(_ctx: Context<ClaimCompetitionContext>) -> Result<()> {
